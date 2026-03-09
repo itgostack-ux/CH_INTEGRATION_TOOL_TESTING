e7060cd0611bfd5ab733efaf08d6419dfd21f447
itgostack-ux <it.gostack@gmail.com>
2026-03-09 18:00:10 +0530

    Added gitignore

---
+++ testerp/gostackerp/doctype/ci_cd_integration_tools/ci_cd_integration_tools.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Gostack and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CI CD Integration Tools", {
+// 	refresh(frm) {
+
+// 	},
+// });
